b839090c306532a479bdeb357a5b144b6194f4ec
Eli Cikowski <disillusionment@gmail.com>
2017-07-27 18:24:05 -0700

    Cleaned up formatting

--- base.c
+++ base.c
@@ -24,14 +24,16 @@
 #define ANSICLR  "\x1B[0;0H\x1B[2J"
 
 
-int MapDisplayWidth  = 7;
-int MapDisplayHeight = 7; 
-int PlayerXLocation = 8;
-int PlayerYLocation = 10;
+int MapDisplayWidth  = 13;
+int MapDisplayHeight = 13; 
+int PlayerXLocation  =  8;
+int PlayerYLocation  = 10;
 
 void clearScreen(void) {
-printf(ANSICLR);
+    printf(ANSICLR);
 }
+
+
 
 void printOffset(int spaces) {
 
@@ -49,163 +51,161 @@ void printFrameTB(void) {
 }
 
 void displayMap(int Width, int Height, int CenterX, int CenterY) {
-     // CenterX-6 to CenterX+6
-     // CenterY-6 to CenterY+6
-     int IterateX, IterateY = 0;
-     int XRange = ((MapDisplayWidth -1) / 2);
-     int YRange = ((MapDisplayHeight -1) / 2);
+    // CenterX-6 to CenterX+6
+    // CenterY-6 to CenterY+6
+    int IterateX, IterateY = 0;
+    int XRange = ((MapDisplayWidth -1) / 2);
+    int YRange = ((MapDisplayHeight -1) / 2);
 
-     // display border
-     clearScreen();
-     printOffset(10);
-     printFrameTB();
+    // display border
+    clearScreen();
+    printOffset(10);
+    printFrameTB();
 
-// TODO
-// if PlayerX < XRange or > MapWidth - Xrange move player instead of map
-// same with PlayerY
+    // TODO
+    // if PlayerX < XRange or > MapWidth - Xrange move player instead of map
+    // same with PlayerY
      
-     // MAP MOVE
-     for(IterateX = CenterX - XRange ; IterateX <= CenterX + XRange ; IterateX++) {
-          printOffset(10);
-          printf(ANSIBLU "|" ANSINRM);
-          for(IterateY = CenterY - YRange ; IterateY <= CenterY + YRange ; IterateY++) {
+    // MAP MOVE
+    for(IterateX = CenterX - XRange ; IterateX <= CenterX + XRange ; IterateX++) {
+        printOffset(10);
+        printf(ANSIBLU "|" ANSINRM);
+        for(IterateY = CenterY - YRange ; IterateY <= CenterY + YRange ; IterateY++) {
        
-               if(IterateX == CenterX && IterateY == CenterY) {
-                    printf(" @");
-               }  else  { 
-                    printf(" %c",map[IterateX][IterateY]);
-               } 
-          }
-          printf(ANSIBLU "|\n" ANSINRM);
-     }
-     // END MAP MOVE
+            if(IterateX == CenterX && IterateY == CenterY) {
+                printf(" @");
+            }  else  { 
+                printf(" %c",map[IterateX][IterateY]);
+            } 
+        }
+        printf(ANSIBLU "|\n" ANSINRM);
+    }
+    // END MAP MOVE
      
-     // PLAYER MOVES MAP DOESN'T
-     // Figure out where @ goes.
+    // PLAYER MOVES MAP DOESN'T
+    // Figure out where @ goes.
      
      
-     // print rest of map.
-     // END OF PLAYER MOVES
-     printOffset(10);
-     printFrameTB();
+    // print rest of map.
+    // END OF PLAYER MOVES
+    printOffset(10);
+    printFrameTB();
 }
 
 void displayStats(void) {};
 
 char getInput(void) {
-     int kbin;
-     printf("> ");
-     system ("/bin/stty raw");
-     kbin = getchar();
-     system ("/bin/stty cooked");
-     return(kbin);
+    int kbin;
+    printf("> ");
+    system ("/bin/stty raw");
+    kbin = getchar();
+    system ("/bin/stty cooked");
+    return(kbin);
 }
 
 
 void displayScreen(void) {
-     displayMap(MapDisplayWidth, 
-                MapDisplayHeight, 
-                PlayerXLocation,
-                PlayerYLocation);
+    displayMap(MapDisplayWidth, 
+               MapDisplayHeight, 
+               PlayerXLocation,
+               PlayerYLocation);
 
-   displayStats();
-
-
-
+    displayStats();
 }
 
 
 
 void impassable(void) {
-   printf("Impassable!");
+    printf("Impassable!");
 }
 
 int passable(int checkMapX, int checkMapY) {
-     char checkPass = map[checkMapX][checkMapY];
-     if( (checkPass != 'X') &&
-         (checkPass != 'x'))
-     {
-          return(1);
-     }  else  {
-          return(0);
-     }
+    char checkPass = map[checkMapX][checkMapY];
+    if( (checkPass != 'X') &&
+        (checkPass != 'x'))
+    {
+         return(1);
+    }  else  {
+         return(0);
+    }
 }
 
 int main(int argc, char *argv[]) {
-     int action;
-     while(1) {
-          displayScreen();
-          action = getInput();
-               switch(action) {
-		  case '4':
-		       if(passable(PlayerXLocation, PlayerYLocation - 1)) {
-                            PlayerYLocation--;
-		       }  else  {
-                            impassable();
-                       }
-                       break;
-		  case '1':
-		       if(passable(PlayerXLocation,PlayerYLocation-1) && 
-                          passable(PlayerXLocation+1,PlayerYLocation)) {
-                            PlayerYLocation--;
-		            PlayerXLocation++;
-		       }  else  {
-                            impassable();
-                       }
-                       break;
-		  case '2':
-                       if(passable(PlayerXLocation+1,PlayerYLocation)) {
-		            PlayerXLocation++;
-		       }  else  {
-                            impassable();
-                       }
-                       break;
-		  case '3':
-		       if(passable(PlayerXLocation, PlayerYLocation+1) && 
-                          passable(PlayerXLocation+1, PlayerYLocation)) {
-                            PlayerYLocation++;
-		            PlayerXLocation++;
-		       }  else  {  impassable(); } break;
-		  case '6':
-                       if(passable(PlayerXLocation, PlayerYLocation+1)) {
-		            PlayerYLocation++;
-		        }  else  {
-                             impassable();
-                        }
-                        break;
-		  case '9':
-                       if(passable(PlayerXLocation, PlayerYLocation+1) && 
-                          passable(PlayerXLocation-1, PlayerYLocation)) {
-		            PlayerYLocation++;
-		            PlayerXLocation--;
-		       }  else  {  
-                            impassable(); 
-                       }
-                       break;
-		  case '8':
-                       if(passable(PlayerXLocation-1,PlayerYLocation)) {
-		            PlayerXLocation--;
-		       }  else  {
-                            impassable(); 
-                       }
-                       break;
-		  case '7':
-                       if(passable(PlayerXLocation, PlayerYLocation-1) && 
-                          passable(PlayerXLocation-1,PlayerYLocation)) {
-		            PlayerYLocation--;
-		            PlayerXLocation--;
-		       }  else  {  
-                            impassable(); }
-                       break;
-		  case 'q':
-		       exit(0);
-		       break;
-                  default:
-		       printf(ANSICYN "\n Unknown Command.\n");
-                       sleep(1);
-                       break;
-		  }
-
-
-          }
+    int action;
+    while(action != 'q') {
+        displayScreen();
+        action = getInput();
+        switch(action) {
+            case '4':
+                if( passable(PlayerXLocation, PlayerYLocation - 1)) {
+                    PlayerYLocation--;
+                }  else  {
+                    impassable();
+                }
+                break;
+            case '1':
+                if( passable(PlayerXLocation,PlayerYLocation-1) && 
+                    passable(PlayerXLocation+1,PlayerYLocation)) {
+                    PlayerYLocation--;
+                        PlayerXLocation++;
+                    }  else  {
+                        impassable();
+                    }
+                    break;
+            case '2':
+                if( passable(PlayerXLocation+1,PlayerYLocation)) {
+                    PlayerXLocation++;
+                }  else  {
+                    impassable();
+                }
+                break;
+            case '3':
+                if( passable(PlayerXLocation, PlayerYLocation+1) && 
+                    passable(PlayerXLocation+1, PlayerYLocation)) {
+                    PlayerYLocation++;
+                    PlayerXLocation++;
+                }  else  {  
+                    impassable(); 
+                } 
+                break;
+            case '6':
+                if( passable(PlayerXLocation, PlayerYLocation+1)) {
+                    PlayerYLocation++;
+                }  else  {
+                    impassable();
+                }
+                break;
+            case '9':
+                if( passable(PlayerXLocation, PlayerYLocation+1) && 
+                      passable(PlayerXLocation-1, PlayerYLocation)) {
+                    PlayerYLocation++;
+                    PlayerXLocation--;
+                   }  else  {  
+                        impassable(); 
+                   }
+                   break;
+              case '8':
+                if( passable(PlayerXLocation-1,PlayerYLocation)) {
+                    PlayerXLocation--;
+                }  else  {
+                    impassable(); 
+                }
+                break;
+            case '7':
+                if( passable(PlayerXLocation, PlayerYLocation-1) && 
+                    passable(PlayerXLocation-1,PlayerYLocation)) {
+                        PlayerYLocation--;
+                        PlayerXLocation--;
+                   }  else  {  
+                        impassable(); }
+                   break;
+            case 'q':
+                   
+                break;
+            default:
+                printf(ANSICYN "\n Unknown Command.\n");
+                sleep(1);
+                break;
+        }
+    }
 }
